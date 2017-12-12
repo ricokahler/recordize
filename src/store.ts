1e559f295501ae0c31643a510323dbdc791a7db0
Rico Kahler <ricokahler@me.com>
2017-12-12 18:32:29 -0500

    new components use newest version of the store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import * as Immutable from 'immutable';
-import { merge } from 'lodash';
+import { merge, isEqual } from 'lodash';
 import * as React from 'react';
 import * as Rx from 'rxjs';
 
@@ -8,9 +8,16 @@ interface Adapter<Store, Props = {}, StoreAdaptedState = {}> {
   set: (store: Store, value?: StoreAdaptedState, props?: Props) => Store,
 }
 
-export function createStore<Store extends Immutable.Record<any>>(t: Store) {
+export function createStore<Store extends Immutable.Record<any>>(initialStore: Store) {
   const updateStream = new Rx.Subject<(previousStore: Store) => Store>();
-  const stateStream = updateStream.scan((state, update) => update(state), t);
+
+  let currentState = initialStore;
+
+  const stateStream = updateStream.scan((store, update) => {
+    const state = update(store);
+    currentState = state;
+    return state;
+  }, initialStore).share();
 
   function connect<Props = {}, State = {}, StoreAdaptedState = {}>(
     adapter: Adapter<Store, Props, StoreAdaptedState>
@@ -18,19 +25,24 @@ export function createStore<Store extends Immutable.Record<any>>(t: Store) {
     class ComponentClass extends React.Component<Props, State & StoreAdaptedState> {
 
       storeSubscription: Rx.Subscription | undefined;
-      _updateStream = updateStream;
-      stateStream = stateStream;
 
       constructor(props: Props, context?: any) {
         super(props, context);
-        this.state = adapter.get(t, this.props) as any as Readonly<State & StoreAdaptedState>;
+        this.state = adapter.get(currentState, this.props) as any as Readonly<State & StoreAdaptedState>;
       }
 
       componentDidMount() {
-        this.storeSubscription = stateStream.subscribe(store => {
-          const stateToSet = adapter.get(store, this.props);
-          this.setState(previousState => ({ ...(previousState as any), ...(stateToSet as any) }));
-        });
+        if (!this.storeSubscription) {
+          this.storeSubscription = (stateStream
+            .subscribe(store => {
+              const stateToSet = adapter.get(store, this.props);
+              this.setState(previousState => ({
+                ...(previousState as any),
+                ...(stateToSet as any),
+              }));
+            })
+          );
+        }
       }
 
       componentWillUnmount() {
@@ -39,13 +51,14 @@ export function createStore<Store extends Immutable.Record<any>>(t: Store) {
         }
       }
 
-      setStore(update: (previousState: StoreAdaptedState) => StoreAdaptedState) {
-        updateStream.next(previousStore => {
+      setStore(updateAdaptedState: (previousState: StoreAdaptedState) => StoreAdaptedState) {
+        const update = (previousStore: Store) => {
           const adaptedStoreState = adapter.get(previousStore, this.props);
-          const updatedStoreState = update(adaptedStoreState);
+          const updatedStoreState = updateAdaptedState(adaptedStoreState);
           const newStore = adapter.set(previousStore, updatedStoreState, this.props);
           return newStore;
-        });
+        };
+        updateStream.next(update);
       }
 
       setGlobalStore(update: (previousStore: Store) => Store) {
