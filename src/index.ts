69fedc7223f8d18e84e8184ac769f0560d7246a4
Rico Kahler <ricokahler@me.com>
2018-01-02 02:05:32 -0500

    remove graph API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from './graph';
 export * from './store';
 export * from './record';
 
--- src/record.ts
+++ src/record.ts
@@ -3,7 +3,6 @@ import * as Immutable from 'immutable';
 export function define<T>(recordDefault: T) {
   const BaseRecordClass: new (t?: Partial<T>) => Immutable.Record<T> = Immutable.Record(recordDefault);
   class RecordClass extends BaseRecordClass {
-    static _recordDefault = recordDefault
   }
-  return RecordClass as any as new (t?: Partial<T>) => Immutable.Record<T> & Readonly<T> & RecordClass;
+  return RecordClass as new (t?: Partial<T>) => Immutable.Record<T> & Readonly<T> & RecordClass;
 }
